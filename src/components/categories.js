38015fde4a15d163df8078da46fafe000b753865
Olya Rykova <oli.andrv96@gmail.com>
2026-05-29 20:34:02 +0200

    add categories

---
+++ src/components/categories.js
@@ -0,0 +1,82 @@
+import { SectionHeader } from "./SectionHeader.js";
+
+export function Categories() {
+  return `
+    <categories class="categories">
+      <div class="categories__container container">
+          <div class="categories__inner">
+            <div class="categories__main">
+                ${SectionHeader("Популярные категории", "смотреть всё")}
+                <ul class="categories__list">
+                    <li class="categories__item">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons.jpg" alt="ballons">
+                            </div>
+                            <p class="categories__item-title">Баблобоксы</p>
+                        </a>
+                    </li>
+                    <li class="categories__item">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                              <img src="./src/images/catalog/ballons.jpg" alt="ballons">
+                            </div>
+                            <p class="categories__item-title">Фонтаны с цифрами</p>
+                        </a>
+                    </li>
+                    <li class="categories__item">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons.jpg" alt="ballons">
+                            </div>    
+                            <p class="categories__item-title">Готовые композиции</p>
+                        </a>
+                    </li> 
+                    <li class="categories__item categories__item--hidden">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons.jpg" alt="ballons">
+                            </div>    
+                            <p class="categories__item-title">Шарики, наборы, оформление комнаты на выписку</p>
+                        </a>
+                    </li>
+                    <li class="categories__item">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons2.jpg" alt="ballons">
+                            </div>    
+                            <p class="categories__item-title">Латексные шары</p>
+                        </a>
+                    </li>
+                    <li class="categories__item">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons2.jpg" alt="ballons">
+                            </div>    
+                            <p class="categories__item-title">Шарики на день рождения</p>
+                        </a>
+                    </li>
+                    <li class="categories__item">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons2.jpg" alt="ballons">
+                            </div>    
+                            <p class="categories__item-title">Фольгированые шары</p>
+                        </a>
+                    </li>
+                    <li class="categories__item categories__item--hidden">
+                        <a href="#" class="categories__item-link">
+                            <div class="categories__item-img">
+                                <img src="./src/images/catalog/ballons2.jpg" alt="ballons">
+                            </div>    
+                            <p class="categories__item-title">3D Сферы, Bubble</p>
+                        </a>
+                    </li>                                                                                                                         
+                </ul>
+            </div>
+          </div>
+        </div>       
+      </div>
+    </categories>
+  `;
+}
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,7 @@
 import { Header } from "../components/header.js";
 import { Hero } from "../components/hero.js";
 import { Slider } from "../components/slider.js";
+import { Categories } from "../components/categories.js";
 import { Footer } from "../components/footer.js";
 
 export function Home() {
@@ -8,6 +9,7 @@ export function Home() {
     <div>
       ${Header()}
       ${Hero()}
+      ${Categories()}
       ${Slider()}
       ${Footer()}
     </div>
